d9dc5fe8ffc5414baf70746550c847947dd7e7a3
Luiz Alberto Melin <luizmelin34@gmail.com>
2022-02-22 12:58:51 -0300

    @LuizAlbertoMelin [consumindo API]

--- src/app/cadastrar/cadastrar.component.ts
+++ src/app/cadastrar/cadastrar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserLogin } from '../model/UserLogin';
+import { Usuario } from '../model/Usuario';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-cadastrar',
@@ -7,9 +11,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrarComponent implements OnInit {
 
-  constructor() { }
+  user: Usuario = new Usuario()
+  confirmSenha : string
+  tipoUsuario : string
+  constructor(
+    private authService: AuthService,
+    private router: Router
+    ) { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit() {
+    window.scroll(0,0)
+
+    }
+
+    confirmaSenha(event: any){
+      this.confirmSenha = event.target.value
+    }
+
+    tipoUser(event: any){
+      this.tipoUser = event.target.value
+    }
+
+    cadastrar(){
+      this.user.tipo = this.tipoUsuario
+
+      if(this.user.senha != this.confirmSenha){
+        alert('As senhas estão incorretas')
+      } else {
+        this.authService.cadastrar(this.user).subscribe((resp: Usuario)=> {
+          this.user = resp
+          this.router.navigate(['/entrar'])
+          alert('usuario cadastrado com sucesso')
+        })
+
+      }
+
+    }
 
 }
