a0aceb8f8d7be4abd67a648bc286f1df78f0e547
Luiz Alberto Melin <luizmelin34@gmail.com>
2022-03-03 12:05:01 -0300

    @LuizAlbertoMelin [editar e apagar tema]

--- src/app/tema/tema.component.ts
+++ src/app/tema/tema.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { Tema } from '../model/Tema';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-tema',
@@ -9,16 +11,34 @@ import { environment } from 'src/environments/environment.prod';
 })
 export class TemaComponent implements OnInit {
 
-  constructor(
-    private router: Router
-  ) { }
+  tema: Tema = new Tema
+  listaTemas: Tema[]
+
+  constructor(private router: Router, private temaService: TemaService) { }
 
   ngOnInit() {
-
+    window.scroll(0,0);
     if(environment.token == ''){
       alert('sua seção expirou, faça login novamente')
       this.router.navigate(['/entrar'])
     }
+    this.listarTemas()
   }
 
+  listarTemas(){
+    this.temaService.getAllTemas().subscribe((resp: Tema[])=>{
+      this.listaTemas = resp
+      console.log(this.tema)
+    })
+  }
+
+  cadastrar(){
+    this.temaService.postTema(this.tema).subscribe((resp: Tema)=>{
+      this.tema = resp;
+      alert("tema cadastrado")
+      this.listarTemas()
+      this.tema = new Tema()
+
+    })
+  }
 }
